3c1adad0cb271ee2d2c9a50381b34238648b492e
saheel <saheel.sarker@gmail.com>
2024-11-13 22:13:21 -0700

    THIS VERSION 3!!!

--- src/app/nlp-generator/nlp-generator.component.ts
+++ src/app/nlp-generator/nlp-generator.component.ts
@@ -11,11 +11,16 @@ import { FormsModule } from '@angular/forms'
 })
 export class NlpGeneratorComponent {
   jobsService = inject(JobsService)
+  selectedJob = this.jobsService.selectedJob;
   highlightsForShow = this.jobsService.highlightsForShow;
   isLoading = this.jobsService.isLoading;
   pastedDescription = '';
 
   generateHighlights() {
+    if (this.selectedJob()){
+      this.pastedDescription = this.selectedJob()?.descriptionHtml || " ";
+    }
     this.jobsService.generateHighlights(this.pastedDescription.trim());
+
   }
 }
